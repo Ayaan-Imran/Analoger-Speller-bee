d1e2cf62338146cb99d888efedcc299dbae01f7c
Ayaan Imran <miskiacuberayaan2509@gmail.com>
2021-06-29 15:05:58 +0300

    1/2 parts done
    The file selection part is done. It will allow the user to select a PDF file and then move on to the ask spelling window

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+
+# Change the window color
+Window.clearcolor = get_color_from_hex("#f8edeb")
 
 # Create the screens
+class Greeting(Screen):
+    pass
+
 class SelectFile(Screen):
     pass
 
@@ -14,7 +24,10 @@ class WindowManager(ScreenManager):
 
 # Create app
 class SpellerApp(App):
-    pass
+    pdf_file_path = ""
+
+    def change_window_background(self, hex_color):
+        Window.clearcolor = get_color_from_hex(hex_color)
 
 # Run the App
 SpellerApp().run()
